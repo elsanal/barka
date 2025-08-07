1c18008a0b9f6f483427b06dbb03434cc342e9e1
SANA ALOUTE <elsanal1995@gmail.com>
2025-08-08 07:25:15 +0800

    Cart

--- lib/features/cart/cart_page.dart
+++ lib/features/cart/cart_page.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class CartPage extends StatefulWidget {
   const CartPage({super.key});
@@ -10,15 +12,15 @@ class CartPage extends StatefulWidget {
 class _CartPageState extends State<CartPage> {
   final TextEditingController _controller = TextEditingController();
   final List<String> _messages = [];
+  var categories = [
+    'Tout',
+    'Électroniques',
+    'Alimentaire',
+    'Motorise',
+    'Habillement',
+  ];
 
-  void _sendMessage() {
-    if (_controller.text.trim().isEmpty) return;
-
-    setState(() {
-      _messages.add(_controller.text.trim());
-      _controller.clear();
-    });
-  }
+  int selectedIndex = -1;
 
   @override
   void dispose() {
@@ -28,13 +30,91 @@ class _CartPageState extends State<CartPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Cart')),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
+    return SafeArea(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.start,
         children: [
-          Center(child: Text("Cart"))
+          SizedBox(
+            height: 50.h,
+            child: ListView.separated(
+              padding: EdgeInsets.symmetric(horizontal: 12.w),
+              scrollDirection: Axis.horizontal,
+              itemCount: categories.length,
+              separatorBuilder: (_, __) => SizedBox(width: 10.w),
+              itemBuilder: (context, index) {
+                return ChoiceChip(
+                  label: Text(
+                    categories[index],
+                    style: TextStyle(
+                      color: selectedIndex == index
+                          ? Colors.white
+                          : Colors.black,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  selected: selectedIndex == index,
+                  onSelected: (bool selected) {
+                    setState(() {
+                      selectedIndex = selected ? index : selectedIndex;
+                    });
+                    print(categories[index]);
+                  },
+                  selectedColor: Colors.deepOrange,
+                  backgroundColor: Colors.grey[200],
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16),
+                    side: BorderSide(
+                      color: selectedIndex == index
+                          ? Colors.deepOrange
+                          : Colors.grey.shade400,
+                    ),
+                  ),
+                  // padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
+                  padding: EdgeInsets.only(bottom: 20.h),
+                );
+              },
+            ),
+          ),
+          Column(
+            children: [
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Checkbox(value: true, onChanged: (bool? newValue) {}),
+                  SizedBox(width: 4),
+                  Text("Shop name"),
+                ],
+              ),
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                // crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  CupertinoCheckbox(
+                    value: true,
+                    onChanged: (bool? newValue) {},
+                  ),
+                  SizedBox(width: 4),
+                  SizedBox(
+                    width: 150.w,
+                    child: ClipRRect(
+                      child: AspectRatio(
+                        aspectRatio: 1,
+                        child: Image.asset("assets/images/img10.jpeg"),
+                      ),
+                    ),
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [Text("Sac de meilleure qualite")],
+                  ),
+                ],
+              ),
+            ],
+          ),
         ],
       ),
     );
