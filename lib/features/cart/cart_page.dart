7538b5f38391a96d873754a2347279a3ca3be174
SANA ALOUTE <elsanal1995@gmail.com>
2025-08-10 10:07:55 +0800

    Cart

--- lib/features/cart/cart_page.dart
+++ lib/features/cart/cart_page.dart
@@ -10,110 +10,190 @@ class CartPage extends StatefulWidget {
 }
 
 class _CartPageState extends State<CartPage> {
-  final TextEditingController _controller = TextEditingController();
-  final List<String> _messages = [];
-  var categories = [
+  final categories = const [
     'Tout',
     'Électroniques',
     'Alimentaire',
-    'Motorise',
+    'Motorisé',
     'Habillement',
   ];
 
-  int selectedIndex = -1;
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
+  int selectedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
+      child: Padding(
+        padding: EdgeInsets.all(8.w),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            _buildCategoryChips(),
+            SizedBox(height: 16.h),
+            Expanded(
+              child: ListView.builder(
+                itemCount: 10,
+                itemBuilder: (context, index) {
+                  return _CartItemCard(index: index);
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildCategoryChips() {
+    return SizedBox(
+      height: 50.h,
+      child: ListView.separated(
+        scrollDirection: Axis.horizontal,
+        padding: EdgeInsets.symmetric(horizontal: 12.w),
+        itemCount: categories.length,
+        separatorBuilder: (_, __) => SizedBox(width: 10.w),
+        itemBuilder: (context, index) {
+          final isSelected = selectedIndex == index;
+          return ChoiceChip(
+            label: Text(
+              categories[index],
+              style: TextStyle(
+                color: isSelected ? Colors.white : Colors.black,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            selected: isSelected,
+            onSelected: (_) => setState(() => selectedIndex = index),
+            selectedColor: Colors.deepOrange,
+            backgroundColor: Colors.grey[200],
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(16.r),
+              side: BorderSide(
+                color: isSelected ? Colors.deepOrange : Colors.grey.shade400,
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class _CartItemCard extends StatelessWidget {
+  final int index;
+  const _CartItemCard({required this.index});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(bottom: 12.h),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(8.r),
+        border: Border.all(width: 1, color: Colors.grey),
+      ),
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.start,
         children: [
-          SizedBox(
-            height: 50.h,
-            child: ListView.separated(
-              padding: EdgeInsets.symmetric(horizontal: 12.w),
-              scrollDirection: Axis.horizontal,
-              itemCount: categories.length,
-              separatorBuilder: (_, __) => SizedBox(width: 10.w),
-              itemBuilder: (context, index) {
-                return ChoiceChip(
-                  label: Text(
-                    categories[index],
-                    style: TextStyle(
-                      color: selectedIndex == index
-                          ? Colors.white
-                          : Colors.black,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                  selected: selectedIndex == index,
-                  onSelected: (bool selected) {
-                    setState(() {
-                      selectedIndex = selected ? index : selectedIndex;
-                    });
-                    print(categories[index]);
-                  },
-                  selectedColor: Colors.deepOrange,
-                  backgroundColor: Colors.grey[200],
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(16),
-                    side: BorderSide(
-                      color: selectedIndex == index
-                          ? Colors.deepOrange
-                          : Colors.grey.shade400,
-                    ),
-                  ),
-                  // padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
-                  padding: EdgeInsets.only(bottom: 20.h),
-                );
-              },
+          // Shop header
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8.w),
+            child: ListTile(
+              leading: Checkbox(value: true, onChanged: (_) {}),
+              title: Text("Boutique de la Martinique"),
+              trailing: Icon(Icons.arrow_forward_ios_rounded),
             ),
           ),
-          Column(
-            children: [
-              Row(
-                mainAxisSize: MainAxisSize.max,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Checkbox(value: true, onChanged: (bool? newValue) {}),
-                  SizedBox(width: 4),
-                  Text("Shop name"),
-                ],
-              ),
-              Row(
-                mainAxisSize: MainAxisSize.max,
-                // crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  CupertinoCheckbox(
-                    value: true,
-                    onChanged: (bool? newValue) {},
-                  ),
-                  SizedBox(width: 4),
-                  SizedBox(
-                    width: 150.w,
+          Divider(height: 1, color: Colors.grey[300]),
+          // Product row
+          Padding(
+            padding: EdgeInsets.all(8.w),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CupertinoCheckbox(value: true, onChanged: (_) {}),
+                SizedBox(
+                  width: 200.w,
+                  child: AspectRatio(
+                    aspectRatio: 1,
                     child: ClipRRect(
-                      child: AspectRatio(
-                        aspectRatio: 1,
-                        child: Image.asset("assets/images/img10.jpeg"),
+                      borderRadius: BorderRadius.circular(8.r),
+                      child: Image.asset(
+                        "assets/images/img10.jpeg",
+                        fit: BoxFit.cover,
+                        cacheWidth: 200,
+                        cacheHeight: 200,
                       ),
                     ),
                   ),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [Text("Sac de meilleure qualite")],
+                ),
+                SizedBox(width: 8.w),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Sac de meilleure qualité",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 40.sp,
+                        ),
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      SizedBox(height: 8.h),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "8374 FCFA",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.deepOrange,
+                              fontSize: 30.sp,
+                            ),
+                          ),
+                          _QuantityControl(),
+                        ],
+                      ),
+                    ],
                   ),
-                ],
-              ),
-            ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _QuantityControl extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          IconButton(
+            onPressed: () {
+              print("Remove more");
+            },
+            icon: Icon(Icons.remove_circle_outline_outlined, size: 50.w),
+            splashRadius: 20,
+            iconSize: 60.w,
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8.w),
+            child: Text("1", style: TextStyle(fontSize: 40.sp)),
+          ),
+          IconButton(
+            onPressed: () {
+              print("add more");
+            },
+            icon: Icon(Icons.add_circle_outline_outlined, size: 50.w),
+            splashRadius: 20,
+            iconSize: 60.w,
           ),
         ],
       ),
